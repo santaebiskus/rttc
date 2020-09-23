d0a0170a2930e43e83047885b8ea6b9573e980dc
santaebiskus <santaebiskus@gmail.com>
2020-09-23 17:03:15 +0300

    оптимизация скрипта

--- 1_pinger.sh
+++ 1_pinger.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 i=1
 while true; do
-ping -c1 8.8.8.8 > /dev/null
+pign_t=$(ping -c1 8.8.8.8)
 ping_r=$?
 if [[ "$ping_r" -eq 0 && "$i" = "-1" || "$ping_r" -ne 0 && "$i" = "1" ]]
 then
-    if [ $ping_r -eq 0 ]
-    then
-	echo "$(date +%H:%M:%S-%d-%m-%y) интернет появился" >> journal.txt
-    else
-	echo "$(date +%H:%M:%S-%d-%m-%y) интернет пропал" >> journal.txt
-    fi
+	echo "$(date +%H:%M:%S-%d-%m-%y) $ping_t" >> journal.txt
     let "i *= -1"
 fi
 sleep 5
