3174d7cff25d55e5fefadc501a388fb79449d6ec
santaebiskus <santaebiskus@gmail.com>
2020-09-23 16:55:59 +0300

    ведение журнала

--- 1_pinger.sh
+++ 1_pinger.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 i=1
 while true; do
-ping -c1 8.8.8.8
+ping -c1 8.8.8.8 > /dev/null
 ping_r=$?
 if [[ "$ping_r" -eq 0 && "$i" = "-1" || "$ping_r" -ne 0 && "$i" = "1" ]]
 then
-    echo "$(date +%H:%M:%S-%d-%m-%y)" `ping -c1 8.8.8.8` >> journal.txt
+    if [ $ping_r -eq 0 ]
+    then
+	echo "$(date +%H:%M:%S-%d-%m-%y) интернет появился" >> journal.txt
+    else
+	echo "$(date +%H:%M:%S-%d-%m-%y) интернет пропал" >> journal.txt
+    fi
     let "i *= -1"
 fi
 sleep 5
