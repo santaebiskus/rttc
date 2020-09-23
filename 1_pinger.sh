4b69dcae5610602cd05e1a36e1bd79523117f3f1
santaebiskus <santaebiskus@gmail.com>
2020-09-23 17:07:31 +0300

    скрипт пингера

--- 1_pinger.sh
+++ 1_pinger.sh
@@ -5,7 +5,7 @@ pign_t=$(ping -c1 8.8.8.8)
 ping_r=$?
 if [[ "$ping_r" -eq 0 && "$i" = "-1" || "$ping_r" -ne 0 && "$i" = "1" ]]
 then
-	echo "$(date +%H:%M:%S-%d-%m-%y) $ping_t" >> journal.txt
+    echo "$(date +%H:%M:%S-%d-%m-%y) $ping_t" >> journal.txt
     let "i *= -1"
 fi
 sleep 5
